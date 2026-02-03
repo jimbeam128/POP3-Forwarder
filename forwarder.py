26eeba2483ab1f7d7eddd4dd25275944d9d0e66d
jimbeam128 <jens@hindricksen.de>
2026-02-03 13:20:20 +0100

    Improve error handling for POP3 login failures
    Handle failed POP3 login attempts gracefully by printing a warning and exiting instead of raising an error.

--- forwarder.py
+++ forwarder.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import poplib
 import smtplib
 import time
@@ -82,7 +83,9 @@ for attempt in range(POP3_RETRIES):
         print(f"[WARN] POP3 Login fehlgeschlagen ({attempt+1}): {e}")
         time.sleep(5)
 if not pop_conn:
-    raise RuntimeError("POP3 Login endgültig fehlgeschlagen")
+    #raise RuntimeError("POP3 Login endgültig fehlgeschlagen")
+    print(f"[WARN] POP3 Login endgültig fehlgeschlagen")
+    sys.exit(0)
 
 # ======================
 # UIDLs abrufen
